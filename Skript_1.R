e06fef0d73d72720f4710cd3bd4d32562832793b
Haiyi11 <97690748+Haiyi11@users.noreply.github.com>
2022-02-03 19:20:00 +0100

    Create Skript_1.R

---
+++ Skript_1.R
@@ -0,0 +1,34 @@
+# Haiyi Rong 
+#a)
+summary()
+
+library(ggplot2)
+A <- function(x){
+  describe(x) 
+}
+
+#b)
+B <- function(x){
+  x <- as.factor(x)
+  return(x)
+}
+
+
+#c)
+library(DescTools)
+
+C <- function(x,y){
+  tab <- table(x,y)
+  Assocs(tab)
+  chisq.test(x,y)
+}
+
+#d)
+library(Hmisc) 
+
+D <- function(x){
+  Hmisc::rcorr(as.matrix(x), type="pearson") 
+}
+
+#f)
+ggplot()
