9369476f5316d1c81b7cdc3ce1c670700f8effda
Xinyi-Jing <97693236+Xinyi-Jing@users.noreply.github.com>
2022-02-03 19:48:56 +0100

    Update Skript_1.R
    Xinyi Jing Teil 3

--- Skript_1.R
+++ Skript_1.R
@@ -150,3 +150,45 @@ f5 <- function(x){
   k[is.na(x)] <- NA
   Tafel <- table(k, x)
   Tafel}
+
+
+#Xinyi  Jing
+#Aufgabe 3
+##a)
+A <- function(x){
+  summary(x) 
+}
+#order
+library(psych)
+A <- function(x){
+  describe(x) 
+}
+
+##b)
+B<-function(x){factor(x = character(), levels, labels = levels, ordered = is.ordered(x))
+ return(summary(x))}
+
+##c)
+
+C<-function(x1,x2){
+  t<-table(x1,x2)
+  return(chisq.test(t))}
+ 
+
+##d)
+#Cochran-Mantel-Haenszel Test
+A <- function(x){
+  cmh.test(x1,x2) 
+}
+
+
+
+##e)
+E<-function(x){y<- factor(x, order = TRUE, levels =c('niedrig', 'mittel', 'hoch'))
+summary(y)}
+
+##f)
+boxplot()
+hist()
+barplot()
+ggplot()
