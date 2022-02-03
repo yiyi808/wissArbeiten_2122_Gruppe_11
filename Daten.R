5239da1a1f62a1f8f0693482b71ebe5b75ba5e49
yiyi808 <zjcs123@outlook.com>
2022-02-03 12:04:28 +0100

    Daten
     Die beiden R-code von mir und von Haiyi möchte ich  in diesem einzigen Dokument zusammengefasst.

--- Daten.R
+++ Daten.R
@@ -1,19 +1,27 @@
 ## Aufgabenstellung 3.Teilleistung Gruppenarbeit/GitHub
 ## Teil 1 
-## Yi
+## Haiyi Rong & Yi Sun
+
+# Die beiden R-Codes sind in diesem Dokument zusammengefasst.
+# In (3), (4), (5) haben wir verschiedenen Idee, es muss noch diskutiert werden..
+
 
 set.seed(1)
-# (1)Alter:
+# (1) Alter:
 Alter <- rnorm(100, mean = 25, sd = 2)
 Alter <- round(Alter,0)
 
-# (2)Studienfach:
+
+# (2) Studienfach:
 fach <- c("Statistik", "Data Science", "Mathe", "Informatik") 
 Studienfach <- sample(fach, 100, replace=TRUE, prob=c(0.3, 0.3, 0.25, 0.15))
 
 df <- data.frame(Alter, Studienfach)
 
-# (3)Interesse an Mathematik:
+
+# (3) Interesse an Mathematik*:
+
+############### Yi:
 interesse1 <- factor(1:7, levels = 1:7, ordered = TRUE)
 # Mathematik 
 # Die meisten Menschen, die Mathematik studieren, interessieren sich dafür, daher liegt der Wert bei 5,6,7; 
@@ -21,12 +29,22 @@ interesse1 <- factor(1:7, levels = 1:7, ordered = TRUE)
 # Ich glaube die Wahrscheinlichkeit von der Wert 1:4 klein ist.
 df$Interesse_Math <- sample(interesse1, 100 , prob = c(0.1, 0.2, 0.2, 0.3, 0.6, 0.7, 0.8), replace=TRUE)
 
-# (4)Interesse an Programmieren:
+############### Haiyi:
+
+
+# (4) Interesse an Programmieren*:
+
+############### Yi:
 # Analog zu Mathe.
 interesse2 <- factor(1:7, levels = 1:7, ordered = TRUE)
 df$Interesse_Prog <- sample(interesse2, 100, prob = c(0.1, 0.2, 0.2, 0.3, 0.6, 0.7, 0.8), replace=TRUE)
 
-# (5)Mathe-LK (ja/nein):
+############### Haiyi: 
+
+
+# (5)Mathe-LK (ja/nein)*:
+
+############### Yi:
 # Annahme: 
 # 1 <- Ja, 0 <- Nein
 # Dummy-Variable hier erstellen.
@@ -45,6 +63,9 @@ df$Mathe_LK[df$Studienfach == c("Statistik")] <-
   ifelse((as.numeric(df$Interesse_Math[df$Studienfach == c("Statistik")]) - as.numeric(df$Interesse_Prog[df$Studienfach == c("Statistik")])) >= "3", 1, 0)
 df$Mathe_LK[df$Studienfach == c("Data Science")] <- 
   ifelse((as.numeric(df$Interesse_Math[df$Studienfach == c("Data Science")]) - as.numeric(df$Interesse_Prog[df$Studienfach == c("Data Science")])) >= "3", 1, 0)
+
+############### Haiyi: 
+
 
 
 df
