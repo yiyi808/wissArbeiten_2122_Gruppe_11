2637a95a25a133053735774d83b817686e81a3fb
yiyi808 <zjcs123@outlook.com>
2022-02-03 01:48:28 +0100

    korrigierte Daten_Yi

--- Daten_Yi.R
+++ Daten_Yi.R
@@ -1,5 +1,6 @@
+## Aufgabenstellung 3.Teilleistung Gruppenarbeit/GitHub
 ## Teil 1 
-# Yi
+## Yi
 
 set.seed(1)
 # (1)Alter:
@@ -9,6 +10,8 @@ Alter <- round(Alter,0)
 # (2)Studienfach:
 fach <- c("Statistik", "Data Science", "Mathe", "Informatik") 
 Studienfach <- sample(fach, 100, replace=TRUE, prob=c(0.3, 0.3, 0.25, 0.15))
+
+df <- data.frame(Alter, Studienfach)
 
 # (3)Interesse an Mathematik:
 interesse1 <- factor(1:7, levels = 1:7, ordered = TRUE)
@@ -25,6 +28,8 @@ df$Interesse_Prog <- sample(interesse2, 100, prob = c(0.1, 0.2, 0.2, 0.3, 0.6, 0
 
 # (5)Mathe-LK (ja/nein):
 # Annahme: 
+# 1 <- Ja, 0 <- Nein
+# Dummy-Variable hier erstellen.
 # a) Die Studenten, die ein größeres Interesse an Mathematik haben und die Mathematik studienren,
 #    die in der Schule Mathe-LK hatten.
 #    D.h. Es ist sehr wahrscheinlich, dass die Werte ihres Interesses_Math 5, 6 und 7 sind.
@@ -40,10 +45,10 @@ df$Mathe_LK[df$Studienfach == c("Statistik")] <-
   ifelse((as.numeric(df$Interesse_Math[df$Studienfach == c("Statistik")]) - as.numeric(df$Interesse_Prog[df$Studienfach == c("Statistik")])) >= "3", 1, 0)
 df$Mathe_LK[df$Studienfach == c("Data Science")] <- 
   ifelse((as.numeric(df$Interesse_Math[df$Studienfach == c("Data Science")]) - as.numeric(df$Interesse_Prog[df$Studienfach == c("Data Science")])) >= "3", 1, 0)
-df$Mathe_LK
-# 1 <- "Ja", 0 <- "Nein"
+
 
 df
+str(df)
 setwd("/Users/is/Documents/GitHub/wissArbeiten_2122_Gruppe_11")
 write.csv(x = df,file = "Daten_Yi.csv")
-
+read.csv("/Users/is/Documents/GitHub/wissArbeiten_2122_Gruppe_11/Daten_Yi.csv")
