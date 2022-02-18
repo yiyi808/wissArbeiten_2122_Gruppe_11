5aa358d9ccee87bb37e9076b8aa52a3e80d5f66f
Xinyi-Jing <xinyi19971129@gmail.com>
2022-02-18 12:26:18 +0100

    Create Teil 4  1).R
    Hallo,Jinzhen .Wir haben über die Altersvariable diskutiert und beschlossen, ein Boxplot zu verwenden. Ich habe ihm Farben hinzugefügt. Aber ich fand, dass das von mir gezeichnete Boxplot zu flach aussieht. Haben Sie eine Idee, wie man das optimieren kann?

---
+++ Teil 4  1).R
@@ -0,0 +1,43 @@
+
+Daten <- read.csv("~/GitHub/wissArbeiten_2122_Gruppe_11/Daten.csv")
+View(Daten)
+str(Daten)
+#Gibt es 6 Variablen und Jede Variable hat 100 Werte.
+#Nummer jeder Person£¬Alter ,Studienfach ,Interesse_Math,Interesse_Prog,Mathe_LK
+Alter<-Daten$Alter
+Studienfach<-Daten$Studienfach 
+#Es gibt vier Studienf??cher:Statisitk ,Data Science, Mathe,Informatik.
+Interesse_Math<-Daten$Interesse_Math
+#Interesse_Math haben Sieben Stufen: 1,2,3,4,5,6,7.
+Interesse_Prog<-Daten$Interesse_Prog
+#Interesse_Prog haben Sieben Stufen: 1,2,3,4,5,6,7.
+Mathe_LK<-Daten$Mathe_LK
+#Eine dichotome Variable mit 2 Level :
+#1(bedeutet "ja"),0(bedeutet "Nein")
+
+mod <- function(x) {
+  uniqv <- unique(x)
+  uniqv[which.max(tabulate(match(x, uniqv)))]
+}
+
+
+A <- function(x){
+  print(mod(x))
+  print(summary(x))
+}
+Alter<-Daten$Alter
+A(Alter)
+#[1] 26
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#21.00   24.00   25.00   25.23   26.00   30.00 
+#Das bedeutet, dass die Personen in diesen Daten zwischen 21 und 30 Jahre alt sind. 
+#26-J??hrige sind die meisten.
+boxplot( Alter,border = "red")
+
+
+
+library(ggplot2)
+ggplot(mpg, 
+       aes(x = Studienfach)) + 
+  geom_bar(position = "stack")
+
