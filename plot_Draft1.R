ced9135a8876e85fc0618ba298ec20ca0a5cc599
LJinzhen <97692706+LJinzhen@users.noreply.github.com>
2022-02-19 23:52:53 +0100

    Update plot_Draft1.R

--- plot_Draft1.R
+++ plot_Draft1.R
@@ -34,15 +34,15 @@ A(Alter_ds)
 #21.00   24.00   25.00   24.97   26.00   30.00 
 
 A(Alter_st)
-#[1] 26
+#[1] mod 26
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #21.00   24.00   25.00   25.26   26.00   29.00 
 A(Alter_Info)
-#27
+#mod 27
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #21.00   24.50   26.00   25.64   27.00   28.00 
 A(Alter_ma)
-#[1] 24
+#[1]mod 24
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #24.00   24.00   25.50   25.50   26.75   28.00 
 
