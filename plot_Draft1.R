ccd7e186311a7863f571e9da7e12bebe1b754aa1
Xinyi-Jing <97693236+Xinyi-Jing@users.noreply.github.com>
2022-02-26 01:05:14 +0100

    Update plot_Draft1.R
    Ich habe alle Analysen durchgeführt. Alle unsere Diagramme und Analysen sind integriert, ich habe Ihre Diagrammdatei bearbeitet. Du kannst es dir ansehen. Wenn es kein Problem gibt, können wir es gemeinsam in den Teil 4-Branch einfügen.

--- plot_Draft1.R
+++ plot_Draft1.R
@@ -4,6 +4,19 @@ library(ggplot2)
 library(DescTools)
 library(hrbrthemes)
 library(viridis)
+library(mvtnorm)
+library(rstudioapi)
+library(gld)
+library(lmom)
+library(e1071)
+library(proxy)
+library(expm)
+library(Exact)
+library(DescTools)
+library(rootSolve)
+
+
+
 #Fuer die ganze Daten
 Daten <- read.csv("~/GitHub/wissArbeiten_2122_Gruppe_11/Daten.csv")
 View(Daten)
@@ -120,6 +133,63 @@ ggplot(data = Daten,aes(x =Mathe_LK ,fill=Mathe_LK))+
   ylab("Absolute Haufigkeit")
 
 
+#d)Interesse_Math 
+#Unterteilen Sie das Interesse in drei Stufen: niedrig, mittel und hoch
+intern <- function(x) min(x):max(x)
+f5 <- function(x){
+  Q <- quantile(intern(x), probs = c(1/3, 2/3), na.rm = TRUE)
+  # Klasseneinteilung
+  k<- numeric(length(x))
+  k[x <= Q[2]] <- "niedrig"
+  k[Q[1] <= x & x < Q[2]] <- "mittel"
+  k[x >= Q[2]] <- "hoch"
+  k[is.na(x)] <- NA
+  return(k)
+}
+
+Daten$Skala_m <- f5(Daten$Interesse_Math)
+#Diese Grafik zeigt, wie viele Studenten verschiedener studienfaecher sich für  Mathe von hoch nach niedrig interessieren.
+ggplot(data = Daten,aes(x=Skala_m,fill=Studienfach))+
+  geom_bar(stat = "count",position="dodge")+
+  theme(axis.title.x=element_blank())+
+  ylab("Anzahl")+
+  xlab("Interesse Stufe")+
+  scale_fill_viridis(discrete = T,alpha=0.6,option = "E") +
+  ggtitle("Balkondiagramm des Interesses an der Mathe") +
+  theme_ipsum()
+
+e)Interesse_Prog
+Daten$Skala_p <- f5(Daten$Interesse_Prog)
+#Diese Grafik zeigt, wie viele Studenten verschiedener studienfaecher sich für das Programmieren von hoch nach niedrig interessieren.
+ggplot(data = Daten,aes(x=Skala_p,fill=Studienfach))+
+  geom_bar(stat = "count",position="dodge")+
+  theme(axis.title.x=element_blank())+
+  ylab("Anzahl")+
+  xlab("Interesse Stufe")+
+scale_fill_viridis(discrete = T,alpha=0.6,option = "E") +
+  ggtitle("Balkondiagramm des Interesse an der Programmierung") +
+  theme_ipsum()
+
+
+#########Studienfach  &  Mathe_LK###############
+#####Test zwischen Studienfach  &  Mathe_LK
+C <- function(x,y){
+  tab <- table(x,y)
+  Assocs(tab)
+  
+  chisq.test(x,y)
+}
+
+C(Studienfach,Mathe_LK)
+#Pearson's Chi-squared test
+
+#data:  x and y
+#X-squared = 43.967, df = 3, p-value = 1.534e-09
+#Da p-value kleiner als 0.05 ist,
+#gibt es einen signifikanten Zusammenhang zwischen Studienfach
+#und Math_LK, d.h. die Studienfach ist nicht unabhaengig vom Math_LK.
+
+#####Grafik zwischen Studienfach  &  Mathe_LK
 #Das nachstehende Histogramm zeigt den prozentualen Anteil der Personen in jedem Studienfach,
 #die in der schule Mathe-LK hatte oder nicht.
 data <- Daten%>% group_by(Studienfach,Mathe_LK)%>%
@@ -139,46 +209,106 @@ ggplot(data,aes(x=Studienfach  ,fill=Mathe_LK,y=anzahl))+
 #Das heißt, von denjenigen, die Mathe und Informatik als Studienfach haben, haben die meisten von ihnen Math_LK in der Schule besucht. 
 #Und die Leute, die Statistik und Data Science studieren, haben meistens keinen Math_LK in der Schule besucht.
 
-###### Mathe & Programmierung Interesse######  
-#Unterteilen Sie das Interesse in drei Stufen: niedrig, mittel und hoch
-intern <- function(x) min(x):max(x)
-f5 <- function(x){
-  Q <- quantile(intern(x), probs = c(1/3, 2/3), na.rm = TRUE)
-  # Klasseneinteilung
-  k<- numeric(length(x))
-  k[x <= Q[2]] <- "niedrig"
-  k[Q[1] <= x & x < Q[2]] <- "mittel"
-  k[x >= Q[2]] <- "hoch"
-  k[is.na(x)] <- NA
-  return(k)
-}
 
-Daten$Skala_m <- f5(Daten$Interesse_Math)
-Daten$Skala_p <- f5(Daten$Interesse_Prog)
 
-#Diese Grafik zeigt, wie viele Studenten verschiedener studienfaecher sich für das Programmieren von hoch nach niedrig interessieren.
-ggplot(data = Daten,aes(x=Skala_p,fill=Studienfach))+
-  geom_bar(stat = "count",position="dodge")+
-  theme(axis.title.x=element_blank())+
-  ylab("Anzahl")+
-  xlab("Interesse Stufe")+
-scale_fill_viridis(discrete = T,alpha=0.6,option = "E") +
-  ggtitle("Balkondiagramm des Interesse an der Programmierung") +
-  theme_ipsum()
-
-#Diese Grafik zeigt, wie viele Studenten verschiedener studienfaecher sich für  Mathe von hoch nach niedrig interessieren.
-ggplot(data = Daten,aes(x=Skala_m,fill=Studienfach))+
-  geom_bar(stat = "count",position="dodge")+
-  theme(axis.title.x=element_blank())+
-  ylab("Anzahl")+
-  xlab("Interesse Stufe")+
-  scale_fill_viridis(discrete = T,alpha=0.6,option = "E") +
-  ggtitle("Balkondiagramm des Interesses an der Mathe") +
-  theme_ipsum()
-
+#Grafik zwischen Studienfach & Interesse_Math & Mathe_LK
 Daten$Mathe_LK[Daten$Mathe_LK=="1"] <-"ja"
 Daten$Mathe_LK[Daten$Mathe_LK=="0"] <- "nein"
-#mosaicplot
-mosaic(~Studienfach+Skala_m+Mathe_LK,data=Daten,highlighting = 'Mathe_LK', highlighting_fill=c('#FAFAD2','#DCDCDC'))
-mosaic(~Studienfach+Skala_p+Mathe_LK,data=Daten,highlighting = 'Mathe_LK', highlighting_fill=c('#FAFAD2','#DCDCDC'))
+#mosaicplot zwischen Studienfach & Interesse_Math & Mathe_LK
+Mosaicplot_Math<-mosaic(~Studienfach+Skala_m+Mathe_LK,data=Daten,highlighting = 'Mathe_LK', highlighting_fill=c('#FAFAD2','#DCDCDC'))
+
+#########Interesse_Math  &  Mathe_LK###############
+#####Test zwischen Interesse_Math  &  Mathe_LK
+C(Interesse_Math,Mathe_LK)
+#Pearson's Chi-squared test
+
+#data:  x and y
+#X-squared = 22.778, df = 6, p-value = 0.0008744
+#Da p-value kleiner als 0.05 ist,
+#gibt es einen signifikanten Zusammenhang zwischen Interesse_Math und Mathe_LK, 
+#d.h. die Interesse_Math ist nicht unabhaengig vom Mathe_LK.
+Mosaicplot_Math
+#####Mosaicplot Erklaerung zwischen Interesse_Math  &  Mathe_LK
+#Wenn wir uns die Mosaicplot_Math ansehen, können wir feststellen, 
+#dass die meisten Menschen, die Math_LK in der Schule besucht haben, sich auf diesen Teil des hohen Interesses an Mathematik konzentrieren
+
+
+#########Studienfach  &  Interesse_Math###############
+#Test zwischen Studienfach & Interesse_Math
+C(Studienfach,Interesse_Math)
+#Pearson's Chi-squared test
+
+#data:  x and y
+#X-squared = 31.795, df = 18, p-value = 0.02325
+#Da p-value kleiner als 0.05 ist,
+#gibt es einen signifikanten Zusammenhang zwischen Studienfach
+#und Interesse_Math, d.h. die Studienfach ist nicht unabhaengig vom Interesse_Math.
+Mosaicplot_Math
+#####Mosaicplot Erklaerung zwischen Studienfach & Interesse_Math
+#Wenn wir uns das Mosaikdiagramm ansehen, können wir sehen, dass Mathematik-Studenten den höchsten Anteil an Personen mit einem hohen Interesse an Mathe haben. 
+#An zweiter Stelle steht der Studiengang Informatik, gefolgt vom Studiengang Data Science. Das Studienfach Statistik ist am niedrigsten.
+
+
+
+
+
+#####Mosaicplot zwischen Studienfach & Interesse_Prog & Mathe_LK
+Mosaicplot_Prog<-mosaic(~Studienfach+Skala_p+Mathe_LK,data=Daten,highlighting = 'Mathe_LK', highlighting_fill=c('#FAFAD2','#DCDCDC'))
+
+#########Studienfach  &  Interesse_Prog###############
+#####Test zwischen Studienfach  &  Interesse_Prog
+C(Studienfach,Interesse_Prog)
+#Pearson's Chi-squared test
+
+#data:  x and y
+#X-squared = 58.401, df = 18, p-value = 3.699e-06
+#Da p-value kleiner als 0.05 ist,
+#gibt es einen signifikanten Zusammenhang zwischen Studienfach
+#und Interesse_Math, d.h. die Studienfach ist nicht unabhaengig vom Interesse_Math.
+Mosaicplot_Prog
+#####Mosaicplot Erklaerung zwischen Studienfach  &  Interesse_Prog
+#Wenn wir uns das Mosaicplot_Prog ansehen, können wir sehen, dass Informatik-Studenten den höchsten Prozentsatz an Menschen mit einem hohen Interesse an Programmierung haben. 
+#An zweiter Stelle steht der Studiengang Data Science, gefolgt vom Studiengang Statistik. Das Studienfach Mathematik ist am niedrigsten.
+
+
+
+#########Interesse_Prog  &  Mathe_LK###############
+#####Test zwischen Interesse_Prog  &  Mathe_LK
+C(Interesse_Prog,Mathe_LK)
+
+#Pearson's Chi-squared test
+
+#data:  x and y
+#X-squared = 19.227, df = 6, p-value = 0.003797
+#Da p-value kleiner als 0.05 ist,
+#gibt es einen signifikanten Zusammenhang zwischen Interesse_Prog und Mathe_LK, 
+#d.h. die Interesse_Prog ist nicht unabhaengig vom Mathe_LK.
+Mosaicplot_Prog
+#####Mosaicplot Erklaerung zwischen Interesse_Prog  &  Mathe_LK
+#Wenn wir uns das Mosaikdiagramm ansehen, können wir sehen, 
+#dass die überwiegende Mehrheit der Menschen mit einem hohen Interesse am Programmieren noch nie Math_LK in der Schule genommen hat.
+
+
+#########Alter  &  Mathe_LK###############
+D <- function(x, y){
+  cor.test(x, y, method=c("pearson"))
+}
+D(Alter,Mathe_LK)
+#Pearson's product-moment correlation
+
+#data:  x and y
+#t = 1.0298, df = 98, p-value = 0.3056
+#alternative hypothesis: true correlation is not equal to 0
+#95 percent confidence interval:
+# -0.0948813  0.2939102
+#sample estimates:
+    # cor 
+#0.103465 
+
+# Da p-value grosse als 0.05 ist,
+#gibt es keinen signifikanten Zusammenhang zwischen Alter und Mathe_LK, 
+#Der Korrelationskoeffizient zwischen ihnen ist sehr gering. 
+#Der Korrelationskoeffizient betraegt 0,103465.
+
+
 
